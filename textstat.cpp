a66456b540b8a8ec7597ca5cf403d0182c1a503d
Tsaritsyn <tsarmihail2013@yandex.ru>
2017-02-19 18:19:23 +0300

    Add files via upload
    This program finds N the most frequently words in a text. It takes N from the console, the text  - from the "input.txt" file and put the words into the console.

---
+++ textstat.cpp
@@ -0,0 +1,77 @@
+//counting how many times each word is mentioned in the text and give N the most frequent
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+typedef map < string, int > statistics;
+
+string prepare (const string &s)
+{
+	string res;
+	for (unsigned int i = 0; i < s.size(); i++)
+	{
+		char c = tolower (s[i]);
+		if ((c >= 'a' && c <= 'z') || c == '\'')
+		{
+			res.push_back (c);
+		}
+	}
+	return res;
+}
+
+struct one
+{
+	string word;
+	int count;
+};
+
+bool compare (const one &a, const one &b)
+{
+	return (a.count > b.count);
+}
+
+int main(int argc, char **argv)
+{
+	int n;
+	cin >> n;
+	freopen ("input.txt", "r", stdin);
+	string s;
+	statistics a;
+	while (!feof (stdin))
+	{
+		cin >> s;
+		//cout << "All right\n";
+		string res = prepare (s);
+		if (res != "")
+		{
+			a[res]++;
+		}
+	}
+	
+	vector < one > v;
+	one buf;
+	
+	for (statistics::iterator it = a.begin(); it != a.end(); ++it)
+	{
+		buf.word = it -> first;
+		buf.count = it -> second;
+		v.push_back (buf);
+		//cout << it -> first << " " << it -> second << endl;
+	}
+	
+	sort (v.begin(), v.end(), compare);
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << v[i].word << " - " << v[i].count << endl;
+	}
+	
+	return 0;
+}
+
